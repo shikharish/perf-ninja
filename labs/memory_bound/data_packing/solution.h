0a7fcce47f2362c3356333d5cb73153deacbed09
Shikhar Soni <shikharish05@gmail.com>
2025-12-10 03:46:30 +0530

    data packing
    Signed-off-by: Shikhar Soni <shikharish05@gmail.com>

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -7,14 +7,26 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
+#ifdef SOLUTION
+  float d;
+  long long l : 16;
+  int i : 8;
+  short s : 8;
+  bool b : 1;
+#else
   int i;
   long long l;
   short s;
   double d;
   bool b;
+#endif
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
+// trick to get the size of S
+// template <int N> class X;
+// X<sizeof(S)> x;
 
 void init(std::vector<S> &arr);
 S create_entry(int first_value, int second_value);
